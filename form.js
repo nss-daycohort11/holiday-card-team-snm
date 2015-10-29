81de2cfc383ccc9ea627cd6b888a903f817324bd
seforarodriguez <seforaroriguez13@gmail.com>
2015-10-29 12:55:18 -0500

    Work in progress

---
+++ form.js
@@ -0,0 +1,24 @@
+var WordGroup = (function(wg){
+	var lexicon = [];
+	var input = $("input-text").val();
+	var inputBtn = $("input-btn");
+	// var i = 0;
+
+	languagePicker = function(argument) {
+		inputBtn.click(function(){
+		console.log(inputBtn);
+		parseInt(input);
+		console.log(input);
+			// for (var i = 0;i < input.length; i++) {
+			// 	// lexicon = new lexicon[input[i]]
+			// 	console.log(lexicon);
+			// };
+		});
+	};
+
+
+	return wg;
+
+});
+
+//hello como estas
